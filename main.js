a0d977c61f23a97ff501e895b9d16a1f25555fe9
Jose Carlos Recuero <jose.recuero@gmail.com>
2016-05-20 23:08:48 -0700

    real alien armada

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 (function() {
-    var toLoad = ["images/cat.png",
-                  "images/alienArmada.json",
-                  "fonts/puzzler.otf"];
+    var toLoad = ["images/alienArmada.json",
+                  "sounds/explosion.mp3",
+                  "sounds/music.mp3",
+                  "sounds/shoot.mp3",
+                  "fonts/emulogic.ttf"];
 
     PIXI.utils._saidHello = true;
 
@@ -10,15 +12,19 @@
     g.start();
 
     var gameScene;
-    var cat;
-    var enemies = [];
-    var enemyData = [{x: 64, y: 64, fill: "blue", stroke: "cyan"},
-                     {x: 128, y: 128, fill: "white", stroke: "red"},
-                     {x: 256, y: 200, fill: "yellow", stroke: "black"}, ];
-    var enemySlide;
+    var cannon;
+    var alienData = [{x: 64, y: 64},
+                     {x: 128, y: 128},
+                     {x: 256, y: 200}, ];
+    var alienSlide;
     var cbs = [];
     var bullets = [];
     var canvasRect = {x: 0, y: 0, width: 512, height: 512};
+    var music;
+    var shootSound;
+    var explosionSound;
+    var aliens = [];
+    var message;
 
     function slide(sprite, xDest, yDest, frames, type, yoyo, cb) {
         cbs.push(cb);
@@ -30,24 +36,17 @@
     }
 
     function setup() {
-        // border = g.rectangle(511, 511, "white", "red", 1, 0, 0);
-        var message = g.text("Now playing", "24px puzzler", "red");
-        message.x = 64;
-        message.y = 32;
+        message = g.text("Now playing", "24px emulogic", "red");
+        message.setPosition(64, 32);
 
         gameScene = g.group();
-        cat = g.sprite("images/cat.png");
-        // cat.setPosition(32, 32);
-        // g.breathe(cat, 1.1, 1.1, 10);
-        // g.pulse(cat, 30, 0.5);
-        // cat.vx = 1;
-        // cat.vy = 1;
+        cannon = g.sprite("cannon.png");
 
-        g.arrowControl(cat, 2);
+        g.arrowControl(cannon, 2);
         g.keyboard(32).press = function() {
-            g.shoot(cat,
+            g.shoot(cannon,
                     4.71,
-                    cat.halfWidth,
+                    cannon.halfWidth,
                     0,
                     g.stage,
                     5,
@@ -55,68 +54,56 @@
                     function() {
                         return g.sprite("bullet.png");
                     });
+            shootSound.play();
         };
 
-        gameScene.addChild(cat);
+        gameScene.addChild(cannon);
 
-        enemyData.forEach(function(data) {
-            var enemy = g.rectangle(64, 64, data.fill, data.stroke, 1, data.x, data.y);
-            enemy.name = enemies.length;
-            enemies.push(enemy);
-            gameScene.addChild(enemy);
-            // g.slide(enemy, 512-64, data.y, 128, "smoothstep", true);
-            var enemySlide = slide(enemy, 512-64, data.y, 360, "smoothstep", true,
+        alienData.forEach(function(data) {
+            var alienFrames = ["alien.png", "explosion.png"];
+            var alien = g.sprite(alienFrames);
+            alien.setPosition(data.x, data.y);
+            aliens.push(alien);
+            var alienSlide = slide(alien, 512-64, data.y, 360, "smoothstep", true,
                                function() {
-                                    // if (g.hit(cat, enemy, bounce=true)) {
-                                    // if (g.hitTestRectangle(cat, enemy)) {
-                                    if (g.hit(cat, enemy, true, false, false,
-                                              function(c, p) {
-                                                    var x = 0;
-                                              })) {
-                                        enemySlide.pause();
+                                    if (g.hit(cannon, alien, true)) {
+                                        alienSlide.pause();
                                     } else {
-                                        enemySlide.play();
+                                        alienSlide.play();
                                     }
                                });
         });
-        // enemySlide = g.slide(enemies[1], 512-64, 128, 128, "sine", true, 1000);
-        // enemySlide = slide(enemies[1], 512-64, 128, 360, "smoothstep", true,
-        //                    function() {
-        //                         if (g.hit(cat, enemies[1], bounce=true)) {
-        //                             enemySlide.pause();
-        //                         }
-        //                    });
+
+        music = g.sound("sounds/music.mp3");
+        music.play();
+
+        shootSound = g.sound("sounds/shoot.mp3");
+        shootSound.pan = -0.5
+
+        explosionSound = g.sound("sounds/explosion.mp3");
+        explosionSound.pan = 0.5;
 
         g.state = play;
     }
 
     function play() {
-        g.move(cat);
-        g.contain(cat, g.stage);
+        g.move(cannon);
+        g.contain(cannon, g.stage);
 
-        // var playerHit = false;
-        // enemies.forEach(function(e) {
-        //     if (g.hit(cat, e, bounce=true)) {
-        //         playerHit = true;
-        //     }
-        // });
-        // if (playerHit) {
-        //     enemySlide.pause();
-        // } else {
-        //     enemySlide.play();
-        // }
+        g.move(aliens);
 
         bullets.forEach(function(b) {
             var hit = false;
-            enemies.forEach(function(e) {
+            aliens.forEach(function(e) {
                 if (g.hit(b, e)) {
                     g.remove(e);
-                    var index = enemies.indexOf(e);
-                    enemies.splice(index, 1);
+                    var index = aliens.indexOf(e);
+                    aliens.splice(index, 1);
                     g.remove(b);
                     index = bullets.indexOf(b);
                     bullets.splice(index, 1);
                     hit = true;
+                    explosionSound.play();
                 }
             });
             if (!hit) {
@@ -127,6 +114,10 @@
                 }
             }
         });
+
+        if (aliens.length == 0) {
+            message.text = "GAME OVER";
+        }
 
         cbs.forEach(function(cb) {
             cb();
